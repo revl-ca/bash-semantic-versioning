5777102bbd61055c78c349646837e2e7096362bf
REVL <steve@revl.ca>
2024-02-28 12:56:44 -0500

    Update tool

--- semantic-versioning.sh
+++ semantic-versioning.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-# USAGE: ./semvr [--debug,--print,--stdout,--print-only,--final,--final-only]
+# USAGE: ./semvr [--debug,--print,--stdout,--print-all,--final,--final-all] path1 path2 path3 ...
 #
 # +----------+----------------------------------------------------------+-------+
 # | *        | Anything                                                 | RC    |
@@ -19,8 +19,9 @@ set -eo pipefail
 # |          | and libraries such as documentation generation           |       |
 # +----------+----------------------------------------------------------+-------|
 
+export PATHS=()
 export DEBUG=0
-export SEMVER_PRINT=0
+export SEMVER_PRINT=1
 export SEMVER_PRINT_ONLY=0
 export SEMVER_FINAL=0
 export SEMVER_FINAL_ONLY=0
@@ -32,64 +33,78 @@ parse_args() {
         DEBUG=1;;
       --print | --stdout)
         SEMVER_PRINT=1;;
-      --print-only)
-        SEMVER_PRINT_ONLY=1;;
+      --print-all)
+        SEMVER_PRINT_ALL=1;;
       --final)
+        SEMVER_PRINT=0
+        SEMVER_PRINT_ALL=0
         SEMVER_FINAL=1;;
-      --final-only)
-        SEMVER_FINAL_ONLY=1;;
+      --final-all)
+        SEMVER_FINAL_ALL=1;;
+      *)
+        PATHS+=("$arg");;
     esac
   done
-}
 
-semvr() {
-  local PATCH=0
-  local MINOR=1
-  local MAJOR=0
-  local RC="0"
-
-  CURRENT=$(git describe --tags --abbrev=0 2> /dev/null || echo "0.1.0")
-  HISTORY=$(git --no-pager log --reverse --oneline "$CURRENT"..HEAD | awk '{ $1=""; print }' | sed -e 's/^[[:space:]]*//')
-  IFS="." read -r MAJOR MINOR PATCH <<< "$CURRENT"
-
-  while IFS= read -r MESSAGE; do
-    (( DEBUG )) && echo -e "\e[32m-> Commit:\e[0m $MESSAGE"
-
-    if [[ "$MESSAGE" =~ ^break ]]; then
-      MAJOR=$((MAJOR+1))
-      MINOR=0
-      PATCH=0
-      RC=0
-
-      (( DEBUG )) && echo -e "\e[33m-->\e[0m Major +1"
-    elif [[ "$MESSAGE" =~ ^feat|^perf ]]; then
-      MINOR=$((MINOR+1))
-      PATCH=0
-      RC=0
-
-      (( DEBUG )) && echo -e "\e[33m-->\e[0m Minor +1"
-    elif [[ "$MESSAGE" =~ ^fix|^docs|^style|^refactor|^test|^chore ]]; then
-      PATCH=$((PATCH+1))
-      RC=0
-
-      (( DEBUG )) && echo -e "\e[33m-->\e[0m Patch +1"
-    else
-      RC=$((RC+1))
-
-      (( DEBUG )) && echo -e "\e[33m-->\e[0m RC +1"
-    fi
-  done < <(printf '%s\n' "$HISTORY")
-
-  if [[ "$SEMVER_PRINT" = "1" ]]; then
-    echo "$MAJOR.$MINOR.$PATCH $MAJOR.$MINOR.$PATCH-$RC $MAJOR $MINOR $PATCH $RC"
-  elif [[ "$SEMVER_PRINT_ONLY" = "1" ]]; then
-    echo "$MAJOR.$MINOR.$PATCH-$RC"
-  elif [[ "$SEMVER_FINAL" = "1" ]]; then
-    echo "$MAJOR.$MINOR.$PATCH $MAJOR.$MINOR.$PATCH $MAJOR $MINOR $PATCH"
-  elif [[ "$SEMVER_FINAL_ONLY" = "1" ]]; then
-    echo "$MAJOR.$MINOR.$PATCH"
+  if [[ "${#PATHS[@]}" = "0" ]]; then
+    PATHS+=(".")
   fi
 }
 
+semvr() {
+  local PATHS=("$@")
+  local PATCH=0
+  local MINOR=1
+  local MAJOR=0
+  local RC=0
+
+  for FOLDER in "${PATHS[@]}"; do
+
+    CURRENT=$(git describe --tags --abbrev=0 2> /dev/null || echo "0.1.0")
+    HISTORY=$(git --no-pager log --reverse --oneline "$CURRENT"..HEAD -- "$FOLDER" | awk '{ $1=""; print }' | sed -e 's/^[[:space:]]*//')
+    IFS="." read -r MAJOR MINOR PATCH <<< "$CURRENT"
+
+    (( DEBUG )) && echo -e "\e[33m-> Folder\e[0m $FOLDER ($CURRENT)"
+
+    while IFS= read -r MESSAGE; do
+      (( DEBUG )) && echo -e "\e[32m-> Commit:\e[0m $MESSAGE"
+
+      if [[ "$MESSAGE" =~ ^break ]]; then
+        MAJOR=$((MAJOR+1))
+        MINOR=0
+        PATCH=0
+        RC=0
+
+        (( DEBUG )) && echo -e "\e[33m-->\e[0m Major +1"
+      elif [[ "$MESSAGE" =~ ^feat|^perf ]]; then
+        MINOR=$((MINOR+1))
+        PATCH=0
+        RC=0
+
+        (( DEBUG )) && echo -e "\e[33m-->\e[0m Minor +1"
+      elif [[ "$MESSAGE" =~ ^fix|^docs|^style|^refactor|^test|^chore ]]; then
+        PATCH=$((PATCH+1))
+        RC=0
+
+        (( DEBUG )) && echo -e "\e[33m-->\e[0m Patch +1"
+      else
+        RC=$((RC+1))
+
+        (( DEBUG )) && echo -e "\e[33m-->\e[0m RC +1"
+      fi
+    done < <(printf '%s\n' "$HISTORY")
+
+    if [[ "$SEMVER_PRINT" = "1" ]]; then
+      echo "$MAJOR.$MINOR.$PATCH-$RC"
+    elif [[ "$SEMVER_PRINT_ALL" = "1" ]]; then
+      echo "$MAJOR.$MINOR.$PATCH $MAJOR.$MINOR.$PATCH-$RC $MAJOR $MINOR $PATCH $RC"
+    elif [[ "$SEMVER_FINAL" = "1" ]]; then
+      echo "$MAJOR.$MINOR.$PATCH"
+    elif [[ "$SEMVER_FINAL_ALL" = "1" ]]; then
+      echo "$MAJOR.$MINOR.$PATCH $MAJOR.$MINOR.$PATCH $MAJOR $MINOR $PATCH"
+    fi
+  done
+}
+
 parse_args "$@"
-semvr
+semvr "${PATHS[@]}"
